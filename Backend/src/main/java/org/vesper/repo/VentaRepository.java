cad7f833ba7838fd97277273549f0214ff195e5d
CristianK25 <151091662+CristianK25@users.noreply.github.com>
2025-10-11 15:18:02 -0300

    Add sales module with Auth0 user association

--- Backend/src/main/java/org/vesper/repo/VentaRepository.java
+++ Backend/src/main/java/org/vesper/repo/VentaRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.vesper.entity.Venta;
 
+import java.util.List;
+
 @Repository
 public interface VentaRepository extends JpaRepository<Venta, Long> {
+    List<Venta> findByUsuarioAuth0Id(String usuarioAuth0Id);
 }
